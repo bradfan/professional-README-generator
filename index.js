73d4f8db84d7b493ed8a41f6aafbfa1d8ae5c31c
jcerb <brt.jst@gmail.com>
2021-03-16 18:50:09 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log("Hello");
+// TODO: Include packages needed for this application
+// NPM Module || NPM Package
+const inquirer = require("inquirer");
+// Node Module || Node Package
+const fs = require("fs");
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
